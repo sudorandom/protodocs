54164fe6246a49d8a130fb09eb6ca82b05d2f379
Kevin <kevin@sudorandom.com>
2025-09-25 20:59:46 +0200

    chore: fix dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import typography from '@tailwindcss/typography';
 
 export default {
-  darkMode: 'class',
+  darkMode: 'media',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
